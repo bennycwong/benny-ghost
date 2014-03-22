e184b3d0cc1e274ca23c71fde8059804be4719f3
bennycwong <bwong337@gmail.com>
2014-03-21 22:16:14 -0700

    Commit message

--- config.js
+++ config.js
@@ -43,24 +43,23 @@ config = {
     // ### Production
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
-    production: {
-        url: 'http://my-ghost-blog.com',
-        mail: {},
-        database: {
-            client: 'sqlite3',
-            connection: {
-                filename: path.join(__dirname, '/content/data/ghost.db')
-            },
-            debug: false
-        },
-        server: {
-            // Host to be passed to node's `net.Server#listen()`
-            host: '127.0.0.1',
-            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
-            port: '2368'
-        }
-    },
-
+	production: {  
+		url: 'http://blog.bennycwong.com',
+		database: {
+			client: 'postgres',
+			connection: {
+				host: 'ec2-54-225-101-119.compute-1.amazonaws.com',
+				user: 'ofquzvvqhldphk',
+				password: 'Tqbl6KkBT-nojc9glOq4ZDrORJ', 
+				database: 'dfl74vvnutirci',
+				port: '5432'
+			}
+		},   
+		server: {
+			host: '0.0.0.0',
+			port: process.env.PORT
+		}
+	},
     // **Developers only need to edit below here**
 
     // ### Testing
